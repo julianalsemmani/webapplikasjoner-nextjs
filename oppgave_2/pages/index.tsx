bec8cad0de6af613b0fde739c77a51afd9ec6cc3
John Ferdie Abueg <johnfa@hiof.no>
2022-11-08 12:47:37 +0100

    Start to display students in index page

--- oppgave_2/pages/index.tsx
+++ oppgave_2/pages/index.tsx
@@ -1,20 +1,41 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+
+type Student = {
+  id: string
+  name: string
+  gender: string
+  age: number
+  group: string
+}
 
 const Home: NextPage = () => {
+  const [students, setStudents] = useState<Student[]>([])
+
   useEffect(() => {
     const handler = async () => {
       try {
+        const response = await fetch('/api/students', {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        const result = await response.json()
+        setStudents(result.data)
       } catch (error) {
         console.log(error)
       }
     }
+
     handler()
   }, [])
 
   return (
     <main>
       <h1>Student gruppering</h1>
+      {/*DISPLAY STUDENTS HERE AS A LIST*/}
     </main>
   )
 }
