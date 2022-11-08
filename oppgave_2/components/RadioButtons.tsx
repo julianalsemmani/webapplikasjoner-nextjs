e915bfdd9bb27145638d01f687e5533df670adf5
John Ferdie Abueg <johnfa@hiof.no>
2022-11-09 00:21:23 +0100

    Html tag structure-/Design-changes to match with given resources

--- oppgave_2/components/RadioButtons.tsx
+++ oppgave_2/components/RadioButtons.tsx
@@ -7,43 +7,41 @@ type RadioButtonsProps = {
 export default function RadioButtons({ handleChange }: RadioButtonsProps) {
   return (
     <section>
-      <form>
-        <label htmlFor="nothing">Ingen</label>
-        <input
-          type="radio"
-          id="nothing"
-          name="sortBy"
-          value="nothing"
-          onChange={handleChange}
-        />
+      <label htmlFor="nothing">Ingen</label>
+      <input
+        type="radio"
+        id="nothing"
+        name="sortBy"
+        value="nothing"
+        onChange={handleChange}
+      />
 
-        <label htmlFor="age">Alder</label>
-        <input
-          type="radio"
-          id="age"
-          name="sortBy"
-          value="age"
-          onChange={handleChange}
-        />
+      <label htmlFor="age">Alder</label>
+      <input
+        type="radio"
+        id="age"
+        name="sortBy"
+        value="age"
+        onChange={handleChange}
+      />
 
-        <label htmlFor="gender">Kjønn</label>
-        <input
-          type="radio"
-          id="gender"
-          name="sortBy"
-          value="gender"
-          onChange={handleChange}
-        />
+      <label htmlFor="gender">Kjønn</label>
+      <input
+        type="radio"
+        id="gender"
+        name="sortBy"
+        value="gender"
+        onChange={handleChange}
+      />
 
-        <label htmlFor="class">Klasse</label>
-        <input
-          type="radio"
-          id="class"
-          name="sortBy"
-          value="class"
-          onChange={handleChange}
-        />
-      </form>
+      <label htmlFor="class">Klasse</label>
+      <input
+        type="radio"
+        id="class"
+        name="sortBy"
+        value="class"
+        onChange={handleChange}
+      />
     </section>
   )
 }
