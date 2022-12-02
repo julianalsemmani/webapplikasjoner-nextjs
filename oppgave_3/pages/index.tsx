7d0e7890cc5e545437101bd1a5728192f7f0c195
John Ferdie Abueg <johnfa@hiof.no>
2022-12-02 23:55:21 +0100

    Changes in index-page, fills database and uses Weeks- and WeekCards-component

--- oppgave_3/pages/index.tsx
+++ oppgave_3/pages/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
+import Weeks from '../components/Weeks'
+import WeekCards from '../components/WeekCards'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const handler = async () => {
+      try {
+        const reponse = await fetch('/api/demo', {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    handler()
+  }, [])
+
   return (
     <main>
       <h1>Lunsjkalender</h1>
+      <Weeks />
+      <WeekCards />
     </main>
   )
 }
