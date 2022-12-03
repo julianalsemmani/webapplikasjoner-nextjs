36fc0cae03e157150d04062976e5304ef46b1ac3
John Ferdie Abueg <johnfa@hiof.no>
2022-12-03 12:35:09 +0100

    /weeks now uses Navbar-component

--- oppgave_3/pages/weeks/index.tsx
+++ oppgave_3/pages/weeks/index.tsx
@@ -1,8 +1,10 @@
+import Navbar from '../../components/Navbar'
 import WeekCards from '../../components/WeekCards'
 
 export default function Weeks() {
   return (
     <>
+      <Navbar />
       <WeekCards />
     </>
   )
